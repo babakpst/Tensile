e40af943ab647ae6220e2f2b758232397b4171ac
Babak <bpoursar@amd.com>
2022-04-21 15:22:05 -0400

    changing the workspacecheck predicate to the original form

--- Tensile/Source/lib/include/Tensile/ContractionProblemPredicates.hpp
+++ Tensile/Source/lib/include/Tensile/ContractionProblemPredicates.hpp
@@ -1186,8 +1186,8 @@ namespace Tensile
 
                 virtual bool operator()(ContractionProblem const& problem) const override
                 {
-                    //return problem.d().totalLogicalElements() * value <= problem.workspaceSize();  // bbk uncomment
-                    return 1==1; // test true.
+                    return problem.d().totalLogicalElements() * value <= problem.workspaceSize();  // bbk uncomment
+                    //return 1==1; // test true.
                 }
 
                 virtual bool debugEval(ContractionProblem const& problem,
